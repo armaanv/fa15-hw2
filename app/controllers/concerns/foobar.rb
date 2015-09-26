016efb5324e0c4fd2ef2ebd1980421928d58256e
armaanv <arm0206@gmail.com>
2015-09-25 17:11:52 -0700

    completed

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,12 @@
 class Foobar
-  # ENTER CODE FOR Q2 HERE
+  def initialize(baz)
+    @baz = baz
+  end
+
+  def bar(cat,dat)
+    @cat = cat.to_s
+    @dat = dat[:sat].to_s
+    @cat + @baz + @dat
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,22 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
-    @text = "You are nothing!"
+    @text             = "You are nothing!"
+    @empty_params     = params[:name].blank? && params[:adjective].blank?
+    @name, @adjective = params[:name], params[:adjective]
   end
 
   def age
   end
 
   def person
+    person = Person.new params[:name], params[:age]
+    @introduction = person.introduce
+    @birth_year   = person.birth_year
+    @nickname     = person.nickname
   end
 end
